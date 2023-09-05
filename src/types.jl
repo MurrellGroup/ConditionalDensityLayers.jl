93ac746b13de64f39ae69575f7bb679bfd4237a8
aronStalmarck <aron.stalmarck@gmail.com>
2023-09-05 11:28:56 +0200

    added comments

--- src/types.jl
+++ src/types.jl
@@ -1,1 +1,7 @@
 abstract type AbstractConditionalDensityLayer end
+
+"""
+MyConditionalDensityLayer <: AbstractConditionalDensityLayer should implement the functions:
+- loss(l::MyConditionalDensityLayer, point, conditionvector)
+- sample(l::MyConditionalDensityLayer, conditionvector)
+"""
