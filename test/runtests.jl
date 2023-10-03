4cc19cc1e065d4861f3cc3a12a01946534e740c4
aronStalmarck <aron.stalmarck@gmail.com>
2023-10-03 12:06:16 +0200

    Add MonteCarloMethods

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 using ConditionalDensityLayers
 using Test
 
-@testset "ConditionalDensityLayers.jl" begin
+@testset "ConditionalDensityLayers.jl" verbose=true begin
     # Write your tests here.
+	include("MonteCarloMethods.jl")
 end
