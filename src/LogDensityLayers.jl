b9d27399475e2bf3a1d4954a26e766cfa02cbbb1
aronStalmarck <aron.stalmarck@gmail.com>
2023-10-19 15:57:13 +0200

    add kwdef and standardloss utility

--- src/LogDensityLayers.jl
+++ src/LogDensityLayers.jl
@@ -1,23 +1,21 @@
 module LogDensityLayers
 
 using ConditionalDensityLayers: AbstractConditionalDensityLayer
+using ConditionalDensityLayers.MonteCarloMethods: AbstractMonteCarloMethod, estimate_integral
 using StatsBase: mean
 using Flux
-
 
 export LogDensityLayer
 
 # --- LogDensityLayer --- #
 
-struct LogDensityLayer <: AbstractConditionalDensityLayer
-    point_encoder
-	condition_encoder
-	logprobability_decoder
+@kwdef struct LogDensityLayer{T1, T2, T3} <: AbstractConditionalDensityLayer where {T1, T2, T3}
+    point_encoder::T1 = identity
+	condition_encoder::T2 = identity
+	logprobability_decoder::T3
 end
 
 Flux.@functor LogDensityLayer
-
-LogDensityLayer(; point_encoder, condition_encoder, logprobability_decoder) = LogDensityLayer(point_encoder, condition_encoder, logprobability_decoder)
 
 (l::LogDensityLayer)(point, condition) = vcat(l.point_encoder(point), l.condition_encoder(condition)) |> l.logprobability_decoder
 
@@ -29,6 +27,10 @@ normalizationloss(∫f) = Flux.mse(log.(∫f), Float32(0))
 
 standardloss(; logp, ∫f) = truesampleloss(logp, ∫f) + normalizationloss(∫f)
 
+standardloss(l::LogDensityLayer, point, condition, montecarlomethod::AbstractMonteCarloMethod, montecarlo_n=1) = 
+	standardloss(logp = l(point, condition), ∫f = estimate_integral(l, condition, montecarlomethod, montecarlo_n))
+
+	
 # --- Utilities --- #
 
 function deepNN(;din, dout, demb::Integer = 16, nhidden::Integer = 1, σ = relu, dropout = 0.0f0)
