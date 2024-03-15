850e18c3fe159b7df6fcb50125213becf4a93610
Lukas Billera <125421968+billera@users.noreply.github.com>
2024-03-15 17:32:43 +0100

    Temporarily mod out logdensitylayers
    Bug with precompilation when importing package

--- src/ConditionalDensityLayers.jl
+++ src/ConditionalDensityLayers.jl
@@ -2,7 +2,7 @@ module ConditionalDensityLayers
 
 # Write your package code here.
 include("types.jl")
-include("LogDensityLayers.jl")
+#include("LogDensityLayers.jl")
 include("GaussianMixtureLayers.jl")
 include("VonMisesMixtureLayers.jl")
 
